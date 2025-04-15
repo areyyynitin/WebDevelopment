d345ae779324bd1b0282617449da3aeff55fe692
niiiit <101655868+niiiit@users.noreply.github.com>
2025-04-15 08:57:20 +0530

    changes week 2

--- week-2/function.js
+++ week-2/function.js
@@ -11,9 +11,20 @@ function sum(n){
 let answer = sum(6);
 // console.log(answer)
 
-const a = require("fs");
+const a = require("fs"); 
 const contents = a.readFileSync("notes.txt" , "utf-8");
-console.log(contents)
+console.log(contents + " ")
 
+function summ(n){
+    let ans = 0;
+    for(let i=0 ; i<=n ; i++){
+        ans = ans +i;
+    }
+    return ans;
+    
+    // return n*(n+1)/2;
+}
+
+let answers = summ(6);
 
 
