b773422bd24796e539edbf96ee3d5a2f8f3fbcb8
areyyynitin <np089250@gmail.com>
2025-04-16 12:48:36 +0000

    class in js

--- week-2/setTimeout.js
+++ week-2/setTimeout.js
@@ -5,8 +5,15 @@ function timeout(){
 }
 timeout();
 setTimeout(timeout , 2000);
-console.log("Good evening");
+
+let c = 0
+for(let i =0 ; i<10;i++){
+    c=c+1; 
+}
+console.log("Good evening"); 
 
 // 1 line will be print
 // in setTimeout function was called but it wait in webApis to ccomplete their timeout,after then he go in callback queue then it transfer into call stack setTimeout called timeout function then it display timeout function
-// between the timeout the next will be print which is good evening 
+// between the timeout the next will be print which is good evening  
+
+ 
