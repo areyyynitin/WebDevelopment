fe57fd737e4473deade4e0cefc9f6636ca2d549a
niiiit <101655868+niiiit@users.noreply.github.com>
2025-04-22 23:27:14 +0530

    half assignment -1 done

---
+++ Practice/week-4/assignment-1/index.js
@@ -0,0 +1,40 @@
+const fs = require("fs")
+// const command = require("commander");
+
+// function viewFile(filename){
+//     return new Promise(readFile);
+// }
+
+// function readFile(resolve){
+//    return   fs.readFile("file.txt","utf-8" , (err,data) => {
+//         resolve(data);
+//     })
+
+// }
+// let word = 1;
+// function printFile(content){
+//     for(let i =0 ; i<content.length;i++){
+//         if(content[i] === " "){
+//             word++;
+//         }
+//     }
+//     console.log(`You have ${word} words in this file`);
+// }
+
+// viewFile().then(printFile);
+
+
+function readFile(filename){
+    fs.readFile(filename,"utf-8" , (err,data) => {
+        let total = 1;
+        for(let i=0;i<data.length;i++){
+            if(data[i] === " "){
+                total++;
+            }
+        }
+        console.log(`You have ${total} words in this ${filename}`);
+    })
+
+}
+
+readFile("file.txt")
