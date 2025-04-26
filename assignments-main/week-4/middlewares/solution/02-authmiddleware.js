0c6185efae962c342750460019771f03ca3f4534
areyyynitin <np089250@gmail.com>
2025-04-26 12:47:52 +0000

    auth

--- assignments-main/week-4/middlewares/solution/02-authmiddleware.js
+++ assignments-main/week-4/middlewares/solution/02-authmiddleware.js
@@ -6,7 +6,7 @@ const VALID_API_KEY = '100xdevs_cohort3_super_secret_valid_api_key'; // key is 1
 function authenticateAPIKey(req, res, next) {
     const apiKey = req.header('100xdevs-api-key'); // Get API key from headers
 
-    if (apiKey && apiKey === VALID_API_KEY) {
+    if (apiKey && (apiKey === VALID_API_KEY)) {
         return next(); // API key is valid, proceed to the next middleware/route handler
     } else {
         return res.status(401).json({ message: 'Invalid or missing API key' }); // API key is invalid or missing
