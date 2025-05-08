5fc49fe5e99643f1e661263c5571379ee8f86368
niiiit <101655868+niiiit@users.noreply.github.com>
2025-05-08 12:52:00 +0530

    jwt tokens

--- Practice/week-6/01-authentication.js
+++ Practice/week-6/01-authentication.js
@@ -1,21 +1,22 @@
 const express = require("express")
-
+const jwt = require("jsonwebtoken")
+const JWT_SECRET = "randomName"
 const app = express();
 
 app.use(express.json())
 
 const user = [];
 
-function generateToken() {
-    let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
+// function generateToken() { 
+//     let options = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z', 'A', 'B', 'C', 'D', 'E', 'F', 'G', 'H', 'I', 'J', 'K', 'L', 'M', 'N', 'O', 'P', 'Q', 'R', 'S', 'T', 'U', 'V', 'W', 'X', 'Y', 'Z', '0', '1', '2', '3', '4', '5', '6', '7', '8', '9'];
 
-    let token = "";
-    for (let i = 0; i < 32; i++) {
-        // use a simple function here
-        token += options[Math.floor(Math.random() * options.length)];
-    }
-    return token;
-}
+//     let token = "";
+//     for (let i = 0; i < 32; i++) {
+//         // use a simple function here
+//         token += options[Math.floor(Math.random() * options.length)];
+//     }
+//     return token;
+// }
  
 app.post("/signup" , (req,res) => {
   const username = req.body.username
@@ -45,8 +46,12 @@ app.post("/signin" , (req,res) => {
     }
     
     if(foundUser){
-        const token = generateToken();
-        foundUser.token = token;
+        const token = jwt.sign({
+            username:username
+        }, JWT_SECRET);
+       
+       
+        // foundUser.token = token;
         res.json({
             token:token
         }) 
@@ -57,5 +62,30 @@ app.post("/signin" , (req,res) => {
     }
     console.log(user)
 }) 
+
+
+app.get("/me" , (req,res) => {
+    const token = req.headers.token
+    const decodedInformation = jwt.verify(token,JWT_SECRET);
+    const username = decodedInformation.username
+    let foundUser = null;
+
+    for(let i=0 ; i<user.length;i++){
+        if(user[i].username == username){
+            foundUser = user[i]
+        }
+    }
+
+    if(foundUser){
+        res.json({
+            username: foundUser.username,
+            password:foundUser.password
+        })
+    } else{
+        res.json({
+            token:"Message invalid"
+        })
+    }
+})
 
 app.listen(3000);
