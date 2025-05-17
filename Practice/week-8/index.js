71911543bff13ec6b01ba616bad468d02fada30c
niiiit <101655868+niiiit@users.noreply.github.com>
2025-05-17 10:51:41 +0530

    step 2 - Add skleton done

--- Practice/week-8/index.js
+++ Practice/week-8/index.js
@@ -9,22 +9,34 @@ app.post("/user/signup" , (req,res) => {
     const email = req.body.email;
     const password = req.body.password
     const name = req.body.name
+
+    res.json({
+        message:"User signup"
+    })
 })
 
 app.post("user/login" , (req,res) => {
-
+      res.json({
+        message:"User login"
+    })
 })
 
-app.post("user/puchase-courses" , (req,res) => {
-
+app.post("/user/purchase" , (req,res) => {
+      res.json({
+        message:"User will purchase course"
+    })
 })
 
-app.get("user/view-all-courses" , (req,res) => {
-
+app.get("/courses" , (req,res) => {
+      res.json({
+        message:"User can view all the course that are listed to buy"
+    })
 })
 
 app.get("user/view-courses" , (req,res) => {
-
+      res.json({
+        message:"User can view courses..that they buy"
+    })
 })
 
 app.listen(3000)
