ed6377e9088843cce27fb58b0b7ae7b4ea4ad70e
niiiit <101655868+niiiit@users.noreply.github.com>
2025-06-07 21:22:39 +0530

    06 and 07 done

--- React/07-reactRouter/src/App.jsx
+++ React/07-reactRouter/src/App.jsx
@@ -3,14 +3,13 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
+
 function App() {
-  const [count, setCount] = useState(0)
+  
 
   return (
     <>
-      <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+     
     </>
   )
 }
